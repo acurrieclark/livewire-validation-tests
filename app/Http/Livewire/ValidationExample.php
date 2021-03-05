87d62ed7255f403d34e45e7f43fe0486308e1db9
Alex Currie-Clark <acurrieclark@gmail.com>
2021-03-05 15:44:54 +0000

    Correct attributes property name

--- app/Http/Livewire/ValidationExample.php
+++ app/Http/Livewire/ValidationExample.php
@@ -11,7 +11,7 @@ class ValidationExample extends Component
 {
     public Collection $references;
 
-    protected $attributes = [
+    protected $validationAttributes = [
         'references.*.email' => 'email',
         'references.*.name' => 'reference name',
     ];
